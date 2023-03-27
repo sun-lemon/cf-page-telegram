7518efe7990b2a3ff77b70e79dc2cc87ea0a0fcd
sun-lemon <64244854+sun-lemon@users.noreply.github.com>
2023-03-27 10:39:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,11 +1,11 @@
 export default {
     async fetch(request, env) {
       let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
+      //if (url.pathname.startsWith('/')) {
         url.hostname="web.telegram.org/z/";
         let new_request=new Request(url,request);
         return fetch(new_request);
-      }
+      //}
       // Otherwise, serve the static assets.
       return env.ASSETS.fetch(request);
     }
